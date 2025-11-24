4457c5a63de8f9f03213a59bb9f96d0a4176049e
Shreyas Dewangswami <138751486+dewangshree@users.noreply.github.com>
2025-11-24 15:30:48 +0530

    Update q3.py

--- q3.py
+++ q3.py
@@ -1,10 +1,15 @@
 from mpi4py import MPI
 comm = MPI.COMM_WORLD
+
 r = comm.Get_rank()
-x = 50 if r == 0 else None
-x = comm.bcast(x, 0)
-print("Rank",r,"=>",x)
+
+data = 50 if r == 0 else None   # Only root has data
+data = comm.bcast(data, root=0)
+
+print("Rank", r, "received:", data)
+
 
 #OUTPUT
-Rank 0 => 50
-Rank 1 => 50
+Rank 0 received: 50
+Rank 1 received: 50
+Rank 2 received: 50
